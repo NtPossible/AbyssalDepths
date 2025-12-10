90df1383113fbd90e0493386f4dfb18e4ff58ac7
NtPossible <64660446+NtPossible@users.noreply.github.com.>
2025-12-10 13:00:20 +0100

    suit safe depth and max oxygen no longer hardcoded

--- AbyssalDepths/src/Items/Wearable/ItemDivingSuit.cs
+++ AbyssalDepths/src/Items/Wearable/ItemDivingSuit.cs
@@ -1,9 +1,24 @@
-﻿using Vintagestory.GameContent;
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.Datastructures;
+using Vintagestory.GameContent;
 
 namespace AbyssalDepths.src.Items.Wearable
 {
     public class ItemDivingSuit : ItemWearable
     {
+        public float MaxOxygenFromJson { get; private set; } = -1f;
+        public int SafeDepthFromJson { get; private set; } = -1;
 
+        public override void OnLoaded(ICoreAPI api)
+        {
+            base.OnLoaded(api);
+
+            JsonObject? abyssalDepths = Attributes?["abyssalDepths"];
+            if (abyssalDepths != null && abyssalDepths.Exists)
+            {
+                MaxOxygenFromJson = abyssalDepths["maxOxygen"].AsFloat(-1f);
+                SafeDepthFromJson = abyssalDepths["safeDepth"].AsInt(-1);
+            }
+        }
     }
 }

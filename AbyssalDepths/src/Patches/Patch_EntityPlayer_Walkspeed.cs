8084cb4d443ce97ebcad1cb79820f44c810fa0a6
NtPossible <64660446+NtPossible@users.noreply.github.com.>
2025-12-15 00:14:49 +0100

    Transpiler to properly override swimming and fix jitter bug. Removes need of other patches, schematic changes so they can go into shelves and stuff.

--- AbyssalDepths/src/Patches/Patch_EntityPlayer_Walkspeed.cs
+++ AbyssalDepths/src/Patches/Patch_EntityPlayer_Walkspeed.cs
@@ -7,6 +7,7 @@ namespace AbyssalDepths.src.Patches
     [HarmonyPatch(typeof(EntityPlayer), "GetWalkSpeedMultiplier")]
     public static class Patch_EntityPlayer_WalkSpeed
     {
+        // Increase walk speed by 20% when wearing a full diving suit and if in liquid
         static void Postfix(EntityPlayer __instance, ref double __result)
         {
             SyncedTreeAttribute attribute = __instance.WatchedAttributes;

e76f13834dd1b5f8582a52dc66c6f47226ec13cf
NtPossible <64660446+NtPossible@users.noreply.github.com.>
2025-12-15 15:29:21 +0100

    added basic config

--- AbyssalDepths/AbyssalDepthsModSystem.cs
+++ AbyssalDepths/AbyssalDepthsModSystem.cs
@@ -1,16 +1,48 @@
-﻿using AbyssalDepths.src.Items.Wearable;
+﻿using AbyssalDepths.src.Config;
+using AbyssalDepths.src.Items.Wearable;
 using HarmonyLib;
+using System;
+using System.Collections.Generic;
 using Vintagestory.API.Common;
 
 namespace AbyssalDepths
 {
     public class AbyssalDepthsModSystem : ModSystem
     {
+        public static AbyssalDepthsConfig Config { get; private set; } = new();
+
         public override void Start(ICoreAPI api)
         {
+            TryLoadConfig(api);
+
             api.RegisterItemClass($"{Mod.Info.ModID}:ItemDivingSuit", typeof(ItemDivingSuit));
 
             new Harmony("abyssaldepths.divingsuit").PatchAll();
+        }
+
+        public static void TryLoadConfig(ICoreAPI api)
+        {
+            try
+            {
+                Config = api.LoadModConfig<AbyssalDepthsConfig>("abyssaldepths.json") ?? new AbyssalDepthsConfig();
+                api.StoreModConfig(Config, "abyssaldepths.json");
+            }
+            catch (Exception exception)
+            {
+                api.Logger.Error("AbyssalDepths: Failed to load config, using defaults.", exception);
+                Config = new AbyssalDepthsConfig();
+            }
+        }
+
+        public override void AssetsFinalize(ICoreAPI api)
+        {
+            if (api.Side != EnumAppSide.Server || Config.EnableSchematicCrafting)
+            {
+                return;
+            }
+
+            List<GridRecipe> recipes = api.World.GridRecipes;
+            recipes.RemoveAll(recipe => recipe?.Output?.Code?.Path?.StartsWith("ad-schematic-divinggear") == true);
         }
     }
 }
---
+++ AbyssalDepths/src/Config/AbyssalDepthsConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace AbyssalDepths.src.Config
+{
+    public class AbyssalDepthsConfig
+    {
+        public bool EnablePressure = true;
+        public int BaseSafeDepth = 20;
+        public bool EnableSchematicCrafting = false;
+    }
+}

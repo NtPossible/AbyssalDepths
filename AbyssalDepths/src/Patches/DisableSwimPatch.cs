fd1cb73064576e8ea1fd5c0bc13114ea735587a2
NtPossible <64660446+NtPossible@users.noreply.github.com.>
2026-04-24 10:45:54 +0200

    Replaced transpiler with a much simpler postfix, should fix a crash too

---
+++ AbyssalDepths/src/Patches/DisableSwimPatch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Datastructures;
+using Vintagestory.GameContent;
+
+namespace AbyssalDepths.src.Patches
+{
+    [HarmonyPatch(typeof(EntityBehaviorControlledPhysics), nameof(EntityBehaviorControlledPhysics.ApplyTests))]
+    public static class DisableSwimPatch
+    {
+        public static void Postfix(Entity ___entity)
+        {
+            if (___entity is not EntityPlayer entityPlayer)
+            {
+                return;
+            }
+
+            SyncedTreeAttribute attributes = entityPlayer.WatchedAttributes;
+            if (attributes?.GetBool("abyssalDepthsDisableSwim") == true)
+            {
+                entityPlayer.Swimming = false;
+            }
+        }
+    }
+}

cef79274423d6cf015f1d604f3cdaf7f3902e162
NtPossible <64660446+NtPossible@users.noreply.github.com.>
2026-05-06 16:10:05 +0200

    minor adjustment

---
+++ AbyssalDepths/src/Entities/Behaviors/EntityBehaviorPressure.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vintagestory.API.Common.Entities;
+
+namespace AbyssalDepths.src.Entities.Behaviors
+{
+    public class EntityBehaviorPressure : EntityBehavior
+    {
+        public override string PropertyName() => "Pressure";
+
+        public EntityBehaviorPressure(Entity entity) : base(entity)
+        {
+        }
+
+        // this will fully replace ModSystemDepthPressure in the future but its a placeholder for now
+
+    }
+}
